3e487b3b0c3965b8cec38504c150add1ebdf28b6
zhangsht <zhsht201501102104@gmail.com>
2017-12-05 14:10:50 +0800

    binary search to find kth small number in sorted matrix

--- AlgorithComplete/functions.h
+++ AlgorithComplete/functions.h
@@ -572,6 +572,9 @@ cur = cur->right;
 		return head;
 	}
 
+	/*
+	* find duplicate
+	*/
 	int findDuplicate(vector<int>& nums) {
 		// int len = nums.size();
 		// for (int i = 0; i < len; i++) {
@@ -594,6 +597,31 @@ cur = cur->right;
 			fast = nums[fast];
 		}
 		return slow;
+	}
+
+	/*
+	* find the kth smallest element in the matrix
+	*/
+	int kthSmallest(vector<vector<int>>& matrix, int k) {
+		int n = matrix.size();
+		int left = matrix[0][0];
+		int right = matrix[n - 1][n - 1];
+
+		while (left < right) {
+			int mid = left + (right - left) / 2;
+			int number = 0;
+			for (int i = 0; i < n; i++) {
+				int count = upper_bound(matrix[i].begin(), matrix[i].end(), mid) - matrix[i].begin();
+				number += count;
+			}
+
+			if (number < k) {
+				left = mid + 1;
+			} else {
+				right = mid;
+			}
+		}
+		return left;
 	}
 
 private:
